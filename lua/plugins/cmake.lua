f0184d9370c89fbdd1bd5df01ab19842ecfd1652
Cameron Macgillivary <cameron.macg@icloud.com>
2025-05-04 19:36:31 -0400

    Added lazy loading to cmake

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -1,8 +1,14 @@
 return {
 	"cdelledonne/vim-cmake",
-	config = function()
-vim.keymap.set("n", "<leader>cc", ":!cmake -G Ninja -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug && cmake --build build --config Debug<cr>", {})
-vim.keymap.set("n", "<leader>cx", ":!ninja clean<cr>", {})
-vim.keymap.set("n", "<leader>cr", ":!cmake -G Ninja -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release<cr>", {})
-	end,
+	keys = {
+		{
+			"<leader>cc",
+			":!cmake -G Ninja -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug && cmake --build build --config Debug<cr>",
+		},
+		{ "<leader>cx", ":!ninja clean<cr>" },
+		{
+			"<leader>cr",
+			":!cmake -G Ninja -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release<cr>",
+		},
+	},
 }
