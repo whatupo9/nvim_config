823d9df69344020c7214203d8b73d30a8a891b97
Cameron Macgillivary <cameron.macg@icloud.com>
2025-05-04 19:33:43 -0400

    Added lazy loading and changed keybinds
    - Added lazy loading when any of the keybinds are used
    - Changed <C-r> to <C-g> because <C-r> is redo, which is useful
    - Also formatted tabs

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,13 +1,13 @@
 return {
-	"ThePrimeagen/harpoon",
-	config = function()
-		vim.keymap.set("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>", {})
-		vim.keymap.set("n", "<C-h>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", {})
-		vim.keymap.set("n", "<C-r>", "<cmd>lua require('harpoon.ui').nav_next()<CR>", {})
-		vim.keymap.set("n", "<C-e>", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", {})
-		vim.keymap.set("n", "<C-j>", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", {})
-		vim.keymap.set("n", "<C-k>", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", {})
-		vim.keymap.set("n", "<C-l>", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", {})
-		vim.keymap.set("n", "<C-;>", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", {})
-	end
+  "ThePrimeagen/harpoon",
+  keys = {
+    { "<leader>a", function() require("harpoon.mark").add_file() end },
+    { "<C-h>", function() require("harpoon.ui").toggle_quick_menu() end },
+    { "<C-g>", function() require("harpoon.ui").nav_next() end },
+    { "<C-e>", function() require("harpoon.ui").nav_prev() end },
+    { "<C-j>", function() require("harpoon.ui").nav_file(1) end },
+    { "<C-k>", function() require("harpoon.ui").nav_file(2) end },
+    { "<C-l>", function() require("harpoon.ui").nav_file(3) end },
+    { "<C-;>", function() require("harpoon.ui").nav_file(4) end },
+  },
 }
