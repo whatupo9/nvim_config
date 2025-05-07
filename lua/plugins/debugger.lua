e148353e25d8ca148454e74855f49d447bce13a0
Cameron Macgillivary <cameron.macg@icloud.com>
2025-05-07 16:08:21 -0400

    Fixed nvim-dapui not loading
    Switched lazy = true to keys = <leader>dr

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -96,7 +96,7 @@ return {
 	{
 		"rcarriga/nvim-dap-ui",
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		lazy = true,
+    keys = "<leader>dr",
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
 			dapui.setup({
