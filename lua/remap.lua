ecd61c4985bd4749224963c38d796b36c638e636
Cameron Macgillivary <cameron.macg@icloud.com>
2025-05-07 11:41:16 -0400

    Removed unused remaps and whitespace

--- lua/remap.lua
+++ lua/remap.lua
@@ -25,10 +25,6 @@ vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>d", '"_d')
 vim.keymap.set("v", "<leader>d", '"_d')
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
